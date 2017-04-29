fba4eda9653b19fe28b3d0cac2b5261fcf8447bd
구민준 <koominjun@guminjun-ui-MacBook-Pro.local>
2017-04-29 16:12:31 +0900

    main design

--- StartUp-master/diet-battle-master/battlediet/app/src/main/java/diet/battle/com/battlediet/MainActivity.java
+++ StartUp-master/diet-battle-master/battlediet/app/src/main/java/diet/battle/com/battlediet/MainActivity.java
@@ -1,14 +1,9 @@
 package diet.battle.com.battlediet;
 
 import android.os.Bundle;
-import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-import com.roughike.bottombar.BottomBar;
-import com.roughike.bottombar.OnTabSelectListener;
-
-import diet.battle.com.battlediet.Adapter.MainViewPagerAdapter;
 import diet.battle.com.battlediet.View.CustomViewPager;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,31 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (CustomViewPager) findViewById(R.id.viewPager);
-        textViewTitle = (TextView)findViewById(R.id.titleName);
-        viewPager.setPagingEnabled(false);
 
-        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
-            @Override
-            public void onTabSelected(@IdRes int tabId) {
-                if (tabId == R.id.tab_favorites) {
-                    viewPager.setCurrentItem(0);
-                    textViewTitle.setText("홈");
-                }
-                else if(tabId == R.id.tab_nearby){
-                    viewPager.setCurrentItem(1);
-
-                }
-                else if(tabId == R.id.tab_friends){
-                    viewPager.setCurrentItem(2);
-                    textViewTitle.setText("배틀 페이지");
-
-                }
-            }
-        });
-        MainViewPagerAdapter adapter = new MainViewPagerAdapter(getSupportFragmentManager(), bottomBar.getTabCount());
-        viewPager.setAdapter(adapter);
-        viewPager.setOffscreenPageLimit(3);
     }
 }
